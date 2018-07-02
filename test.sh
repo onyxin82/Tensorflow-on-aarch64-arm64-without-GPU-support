3f880e24f5f7e139552fb6e76c3eac192de40377
huzz <30976291+huzz@users.noreply.github.com>
2018-07-02 05:26:23 +0000

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+apt-get install python3-pip python3-dev git
+echo "                "
+echo "                "
+echo "10% done"
+echo "Installed python dependencies"
+echo "                "
+echo "                "
+echo "                "
+
+git clone https://github.com/huzz/Tensorflow-on-aarch64-arm64-without-GPU-support.git
+
+echo "                "
+echo "                "
+echo "30% done"
+echo "Installing tensorflow this may take a while"
+echo "Please be patient                "
+echo "                "
+echo "                "
+
+cd Tensorflow-on-aarch64-arm64-without-GPU-support
+
+pip3 install tensorflow-1.4.0rc0-cp35-cp35m-linux_aarch64.whl
+
+echo "Installation complete"
